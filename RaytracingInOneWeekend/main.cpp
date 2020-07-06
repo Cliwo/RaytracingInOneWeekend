8c92d903c78e80137f98f4c789364b31ce5f9c55
Chan <cliwo@naver.com>
2020-07-07 02:15:45 +0900

    Chapter 7.

--- RaytracingInOneWeekend/main.cpp
+++ RaytracingInOneWeekend/main.cpp
@@ -2,6 +2,7 @@
 
 #include "hittable_list.h"
 #include "sphere.h"
+#include "camera.h"
 
 #include <iostream>
 color ray_color(const ray& r, const hittable& world) {
@@ -22,34 +23,31 @@ int main() {
 
 	const int image_width = 384;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
 
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-	
-	auto viewport_height = 2.0;
-	auto viewport_width = aspect_ratio * viewport_height;
-	auto focal_length = 1.0;
-
-	auto origin = point3(0, 0, 0);
-	auto horizontal = vec3(viewport_width, 0, 0);
-	auto vertical = vec3(0, viewport_height, 0);
-	auto lower_left_corner = origin - horizontal * 0.5f - vertical * 0.5f - vec3(0, 0, focal_length);
 
 	hittable_list world;
 	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
 	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
-
+    
+    camera cam;
+    
 	for (int j = image_height - 1; j >= 0; j--)
 	{
 		std::cerr << "\nScanlines remaining : " << j << ' ' << std::flush;
 		for (int i = 0; i < image_width; i++)
 		{
-			auto u = double(i) / (image_width - 1);
-			auto v = double(j) / (image_height - 1);
+            color pixel_color(0, 0, 0);
+            for(int k = 0; k < samples_per_pixel ; k++)
+            {
+                auto u = double(i) / (image_width - 1);
+                auto v = double(j) / (image_height - 1);
 
-			ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-			color pixel_color = ray_color(r, world);
-			
-			write_color(std::cout, pixel_color);
+                ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world);
+            }
+			write_color(std::cout, pixel_color, samples_per_pixel);
 		}
 	}
 	std::cerr << "\nDone.\n";
