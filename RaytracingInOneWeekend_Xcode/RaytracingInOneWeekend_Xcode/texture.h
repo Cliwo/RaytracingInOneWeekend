5ffcf9ccc64ad0cebdfb897f2d4881a2b86883a2
Chan <cliwo@naver.com>
2020-08-09 00:39:24 +0900

    Next Week 5.7

--- RaytracingInOneWeekend_Xcode/RaytracingInOneWeekend_Xcode/texture.h
+++ RaytracingInOneWeekend_Xcode/RaytracingInOneWeekend_Xcode/texture.h
@@ -58,7 +58,7 @@ public:
     noise_texture() {}
     noise_texture(double sc) : scale(sc) {}
     virtual color value(double u, double v, const vec3& p) const {
-        return color(1,1,1) * noise.turb(scale * p);
+        return color(1,1,1) * 0.5 * (1 + sin(scale*p.z() + 10*noise.turb(p)));
     }
 public:
     perlin noise;
